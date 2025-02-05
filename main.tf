a8036b10a9d312936d2b848aaa61561459d4d0f4
Jack Mulligan <mulligja1@gmail.com>
2025-02-05 19:24:01 +0000

    Updated terraform config to have uppercase names

--- main.tf
+++ main.tf
@@ -73,17 +73,17 @@ resource "google_storage_bucket_iam_member" "member" {
 
 # SNOWFLAKE INFRA
 resource "snowflake_database" "terraform_demo_db" {
-  name = "${var.sf_project}_db"
+  name = "${var.sf_project}_DB"
   comment = "Terraform demo database"
 }
 
 resource "snowflake_schema" "terraform_demo_schema" {
-  name = "${var.sf_project}_schema"
+  name = "${var.sf_project}_SCHEMA"
   database = snowflake_database.terraform_demo_db.name
 }
 
 resource "snowflake_user" "terraform_demo_user" {
-  name = "${var.sf_project}_user"
+  name = "${var.sf_project}_USER"
   rsa_public_key   = var.sf_project_private_key
   must_change_password = "false"
 }
@@ -94,14 +94,14 @@ resource "snowflake_grant_account_role" "g" {
 }
 
 resource "snowflake_file_format" "terraform_file_format" {
-  name = "${var.sf_project}_file_format"
+  name = "${var.sf_project}_FILE_FORMAT"
   database = snowflake_database.terraform_demo_db.name
   schema = snowflake_schema.terraform_demo_schema.name
   format_type = "PARQUET"
 }
 
 resource "snowflake_storage_integration" "gcs_storage_integration" {
-  name = "${var.sf_project}_gcs_storage_integration"
+  name = "${var.sf_project}_GCS_STORAGE_INTEGRATION"
   type = "EXTERNAL_STAGE"
   storage_provider = "GCS"
   enabled = true
@@ -110,7 +110,7 @@ resource "snowflake_storage_integration" "gcs_storage_integration" {
 }
 
 resource "snowflake_stage" "gcs_external_stage" {
-  name = "${var.sf_project}_external_stage"
+  name = "${var.sf_project}_EXTERNAL_STAGE"
   database = snowflake_database.terraform_demo_db.name
   schema = snowflake_schema.terraform_demo_schema.name
   url = "gcs://${google_storage_bucket.terraform_demo_bucket.name}"
